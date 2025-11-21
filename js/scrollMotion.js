084a3a0d734eed64681cfb24f434f1c3dacd9885
fs-class-n <whyeil@naver.com>
2025-11-21 18:01:55 +0900

    개인사이트

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -5,12 +5,12 @@ $(window).scroll(function () {
     const speed = 1000,
         easing = "easeOutExpo",
         pos = 0,
-        offset = 0.5;
+        offset = 2.5;
 
     // 오른쪽 상단에 스크롤 양과 창 높이 값 출력
     // console.log(windowScrollTop + " , " + windowHeight);
 
-    if (windowScrollTop > windowHeight / offset) {
+    if (windowScrollTop > windowHeight * offset) {
         //참일 경우
         // i는 0, 1, 2, 3
         for (let i = 0; i < 4; i++) {
@@ -29,4 +29,13 @@ $(window).scroll(function () {
             .stop()
             .animate({ top: pos + 600, opacity: 0 }, speed, easing);
     }
-});
+
+    // 창의 스크롤 양이 0보다 크면
+    if (windowScrollTop > 0) {
+        // 헤더 요소에 active 클래스 추가
+        $("#header").addClass("active");
+    } else {
+        // 헤더 요소에 active 클래스 제거
+        $("#header").removeClass("active");
+    }
+}); /* $(window).scroll(); */
