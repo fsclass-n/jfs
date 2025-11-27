e8a685d25d53e3ec66cdbb6a91e05e7bb394cbbd
fs-class-n <whyeil@naver.com>
2025-11-27 18:00:29 +0900

    진행률바

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -6,16 +6,27 @@ $(window).scroll(function () {
     $(".motion").each(function (i, el) {
         // position(): 포지션이 지정된 가장 가까운 조상 요소를 기준으로 지정한다.
         const elementTop = $(el).position().top;
+        // offset1: .motion의 움직이기 시작하는 시점
+        // -> 숫자가 클수록 더 많이 스크롤해야 동작한다.
+        // offset2: .motion의 시작 위치
         let offset1, offset2, speed;
 
-        if (i < 3) {
-            offset1 = 800;
-            offset2 = 600;
-            speed = 1000;
-        } else {
-            offset1 = 1800;
-            offset2 = 600;
-            speed = 400;
+        switch (i) {
+            case 0:
+                offset1 = 800;
+                offset2 = 300;
+                speed = 2000;
+                break;
+            case 1:
+            case 2:
+                offset1 = 800;
+                offset2 = 600;
+                speed = 1000;
+                break;
+            default:
+                offset1 = 1800;
+                offset2 = 600;
+                speed = 400;
         }
 
         // console.log(elementTop);
