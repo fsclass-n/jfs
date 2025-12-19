041b110a64edad90659ae26cde25179450f4bae9
fs-class <whyeil29@gmail.com>
2025-12-19 15:07:43 +0900

    리액트라우터

---
+++ basic/react/vite/my-router/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
