e6337fccb6106f1e5cdfb00ddc4ea4d1020bc02e
fs-class <whyeil29@gmail.com>
2025-12-19 15:23:47 +0900

    리액트 라우터

--- basic/react/vite/my-router/src/App.jsx
+++ basic/react/vite/my-router/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
     );
 }
 
